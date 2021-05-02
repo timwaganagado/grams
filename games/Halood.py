e08e1b3cdb475aa6558e72b35cc5db9c2af1921d
timwaganagado <burton@vanderstok.net>
2021-05-03 09:55:45 +1000

    3/05/2021

--- games/Halood.py
+++ games/Halood.py
@@ -26,7 +26,7 @@ GREY = (128,128,128)
 check = 'working'
 
 pg.init()
-screen = pg.display.set_mode((WIDTH, HEIGHT),pg.FULLSCREEN)
+screen = pg.display.set_mode((WIDTH, HEIGHT),pg.FULLSCREEN,display = 1)
 clock = pg.time.Clock()
 
 def draw_grid():
@@ -37,17 +37,47 @@ def draw_grid():
 
 class main():
     def __int__(self):
-        self.p = 0
+        self.heplanevec = 0
+        self.conriftvec = 0
+        self.current_animation = 0
+        self.heplane_combat_animation = 0
+        self.conrift_combat_animation = 0
     def draw_icons(self):
-        goal_center = (self.p.x * TILESIZE + TILESIZE / 2, self.p.y * TILESIZE + TILESIZE / 2)
-        screen.blit(cross_img, cross_img.get_rect(center=goal_center))
+        ani = M.heplane_combat_animation
+        goal_center = (self.heplanevec.x * TILESIZE + TILESIZE / 2, self.heplanevec.y * TILESIZE + TILESIZE / 2)
+        screen.blit(ani[self.current_animation], ani[self.current_animation].get_rect(center=goal_center))
+        ani = M.conrift_combat_animation
+        goal_center = (self.conriftvec.x * TILESIZE + TILESIZE / 2, self.conriftvec.y * TILESIZE + TILESIZE / 2)
+        screen.blit(ani[self.current_animation], ani[self.current_animation].get_rect(center=goal_center))
 
-cross_img = pg.image.load('images/cross-1.png.png').convert_alpha()
-cross_img = pg.transform.scale(cross_img, (50, 50))
-cross_img.fill((255, 0, 0, 255), special_flags=pg.BLEND_RGBA_MULT)
+heplane_combat_img = pg.image.load('images/Layer 1_heplane_combat1.png').convert_alpha()
+heplane_combat_img = pg.transform.scale(heplane_combat_img, (256, 256))
+heplane_combat2_img = pg.image.load('images/Layer 1_heplane_combat2.png').convert_alpha()
+heplane_combat2_img = pg.transform.scale(heplane_combat2_img, (256, 256))
+heplane_combat3_img = pg.image.load('images/Layer 1_heplane_combat3.png').convert_alpha()
+heplane_combat3_img = pg.transform.scale(heplane_combat3_img, (256, 256))
+
+conrift_combat_img = pg.image.load('images/Layer 1_conrift_combat1.png').convert_alpha()
+conrift_combat_img = pg.transform.scale(conrift_combat_img, (256, 256))
+conrift_combat2_img = pg.image.load('images/Layer 1_conrift_combat2.png').convert_alpha()
+conrift_combat2_img = pg.transform.scale(conrift_combat2_img, (256, 256))
+conrift_combat3_img = pg.image.load('images/Layer 1_conrift_combat3.png').convert_alpha()
+conrift_combat3_img = pg.transform.scale(conrift_combat3_img, (256, 256))
+
+
+
+
 
 M = main()
-M.p = vec(20,20)
+M.heplanevec = vec(20,20)
+M.conriftvec = vec(43,20)
+M.heplane_combat_animation = {1:heplane_combat_img,2:heplane_combat2_img,3:heplane_combat3_img}
+M.conrift_combat_animation = {1:conrift_combat_img,2:conrift_combat2_img,3:conrift_combat3_img}
+
+M.current_animation = 1
+
+
+anim_timer = pg.time.get_ticks()
 
 running = True
 while running:
@@ -58,8 +88,10 @@ while running:
         if event.type == pg.MOUSEBUTTONDOWN:
             if event.button == 1:
                 mpos = vec(pg.mouse.get_pos()) // TILESIZE
-                M.p = mpos
+                M.conriftvec = mpos
         if event.type == pg.KEYDOWN:
+            if event.key == pg.K_m:
+                print(M.conriftvec)
             if event.key == pg.K_ESCAPE:
                 running = False
                 pg.quit
@@ -68,7 +100,12 @@ while running:
         if event.type == pg.QUIT: # allows for quit when clicking on the X 
             running = False
             pg.quit() 
-
+    current_time = pg.time.get_ticks()
+    if current_time - anim_timer > 1000:
+        M.current_animation += 1
+        if M.current_animation == 4:
+            M.current_animation = 1
+        anim_timer = pg.time.get_ticks()
     pg.display.set_caption("{:.2f}".format(clock.get_fps())) # changes the name of the application
     screen.fill(WHITE) # fills screnn with color
     # anything down here will be displayed ontop of anything above
