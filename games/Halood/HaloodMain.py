5ef5ee98624c43c72662416b7ff5b7c098f95a35
burt on <2690506@schoolsnet.act.edu.au>
2021-05-14 14:49:09 +1000

    14/05/2021

--- games/Halood/HaloodMain.py
+++ games/Halood/HaloodMain.py
@@ -89,7 +89,7 @@ C.vec = vec(43,20)
 C.health = 50
 C.combat_animation = {1:conrift_combat_img,2:conrift_combat2_img,3:conrift_combat3_img}
 C.clickaura = [vec(-1,0),vec(-1,1),vec(-1,2),vec(-1,3),vec(-1,-1),vec(-1,-2),vec(-1,-3),vec(0,0),vec(0,1),vec(0,2),vec(0,3),vec(0,-1),vec(0,-2),vec(0,-3),vec(1,0),vec(1,1),vec(1,2),vec(1,3),vec(1,-1),vec(1,-2),vec(1,-3)]
-C.attacks = {'darkness':[4,5],'conduction':[16,1]}
+C.attacks = {'darkness':[5,5],'conduction':[20,1]}
 
 
 C2 = enemy.conrift2()
@@ -97,7 +97,7 @@ C2.vec = vec(43,20)
 C2.health = 50
 C2.combat_animation = {1:conrift_combat_img,2:conrift_combat2_img,3:conrift_combat3_img}
 C2.clickaura = [vec(-1,0),vec(-1,1),vec(-1,2),vec(-1,3),vec(-1,-1),vec(-1,-2),vec(-1,-3),vec(0,0),vec(0,1),vec(0,2),vec(0,3),vec(0,-1),vec(0,-2),vec(0,-3),vec(1,0),vec(1,1),vec(1,2),vec(1,3),vec(1,-1),vec(1,-2),vec(1,-3)]
-C2.attacks = {'darkness':[4,5],'conduction':[16,1]}
+C2.attacks = {'darkness':[5,5],'conduction':[20,1]}
 
 
 
@@ -112,7 +112,7 @@ auras = [(0, 3), (1, 3), (2, 3), (2, 2), (1, 2), (0, 2), (0, 1), (1, 1), (2, 1),
 mage.clickaura = []
 for aura in auras:
     mage.clickaura.append(vec(aura))
-mage.attacks = {'fire ball':[10,3],'miss':[0,1]}
+mage.attacks = {'fire ball':[10,3],'lightning':[15,1],'miss':[0,1]}
 
 mage2 = enemy.magee2()
 mage2.vec = vec(43,20)
@@ -122,7 +122,7 @@ auras = [(0, 3), (1, 3), (2, 3), (2, 2), (1, 2), (0, 2), (0, 1), (1, 1), (2, 1),
 mage2.clickaura = []
 for aura in auras:
     mage2.clickaura.append(vec(aura))
-mage2.attacks = {'fire ball':[10,3],'miss':[0,1]}
+mage2.attacks = {'fire ball':[10,3],'lightning':[15,1],'miss':[0,1]}
 
 def draw_grid():
     for x in range(0, WIDTH, TILESIZE):
@@ -152,55 +152,61 @@ class ally():
                     M.allies[M.selectedchar][1] = 50
         def draw_icons(self):
             for x in self.attacks:
-                icon = self.attacks[x][1]
-                pos = self.attacks[x][2]
-                rect = pg.Rect(int(pos.x*TILESIZE-49), int(pos.y*TILESIZE-50), 128, 128)
-                pg.draw.rect(screen,BLACK,rect)
-                goal_center = (int(pos.x * TILESIZE + TILESIZE / 2), int(pos.y * TILESIZE + TILESIZE / 2))
-                screen.blit(icon, icon.get_rect(center=goal_center))
-                text = str(round(int((M.allies[ally1][1]/100 + 1) * self.attacks[self.attack1][0][0])))
-                draw_text(text, 20, RED, self.attacks[self.attack1][2].x*TILESIZE, self.attacks[self.attack1][2].y*TILESIZE - 50)
-                text = str(round(int((M.allies[ally1][1]/100 + 1) * self.attacks[self.attack2][0][0])))
-                draw_text(text, 20, RED, self.attacks[self.attack2][2].x*TILESIZE, self.attacks[self.attack2][2].y*TILESIZE - 50)
-                if len(ally1.healdam) != 0:
-                    l = 0
-                    for x in ally1.healdam:
-                        l += x
-                    text = str(l)
-                else:
-                    text = str(0)
-                draw_text(text, 20, GREEN, self.attacks[self.attack3][2].x*TILESIZE, self.attacks[self.attack3][2].y*TILESIZE - 50)
+                if H in M.allies: 
+                    icon = self.attacks[x][1]
+                    pos = self.attacks[x][2]
+                    rect = pg.Rect(int(pos.x*TILESIZE-49), int(pos.y*TILESIZE-50), 128, 140)
+                    pg.draw.rect(screen,BLACK,rect)
+                    goal_center = (int(pos.x * TILESIZE + TILESIZE / 2), int(pos.y * TILESIZE + TILESIZE / 2))
+                    screen.blit(icon, icon.get_rect(center=goal_center))
+                    print(M.allies[M.ally1][1])
+                    text = str(round(int((M.allies[M.ally1][1]/100 + 1) * self.attacks[self.attack1][0][0])))
+                    draw_text(text, 20, RED, self.attacks[self.attack1][2].x*TILESIZE, self.attacks[self.attack1][2].y*TILESIZE + 65)
+                    text = str(round(int((M.allies[M.ally1][1]/100 + 1) * self.attacks[self.attack2][0][0])))
+                    draw_text(text, 20, RED, self.attacks[self.attack2][2].x*TILESIZE, self.attacks[self.attack2][2].y*TILESIZE + 65)
+                    if len(ally1.healdam) != 0:
+                        l = 0
+                        for x in ally1.healdam:
+                            l += x
+                        text = str(l)
+                    else:
+                        text = str(0)
+                    draw_text(text, 20, GREEN, self.attacks[self.attack3][2].x*TILESIZE, self.attacks[self.attack3][2].y*TILESIZE + 65)
+        def draw_attack(self):
+            pass
     class sri():
         def __int__(self):
             self.attack1 = 0
         def attack(self,target,attack):
             for x in M.enemy:
-                M.enemy[x][1] += -2
+                M.enemy[x][1] -= self.attacks[self.attack1][0]
         def support(self,target):
             attack = M.selectedattack
             if attack == self.attack2:
-                M.allies[target][3] += 10
+                M.allies[target][3] += self.attacks[self.attack2][0]
                 if M.allies[target][3] > target.health:
                     M.allies[target][3] = target.health
             elif attack == self.attack3:
-                M.allies[target][1] += 10
+                M.allies[target][1] += self.attacks[self.attack3][0]
                 if M.allies[target][1] > target.health:
                     M.allies[target][1] = target.health
         def draw_icons(self):
             for x in self.attacks:
-                icon = self.attacks[x][1]
-                pos = self.attacks[x][2]
-                rect = pg.Rect(int(pos.x*TILESIZE-49), int(pos.y*TILESIZE-50), 128, 128)
-                pg.draw.rect(screen,BLACK,rect)
-                goal_center = (int(pos.x * TILESIZE + TILESIZE / 2), int(pos.y * TILESIZE + TILESIZE / 2))
-                screen.blit(icon, icon.get_rect(center=goal_center))
-                text = str(self.attacks[self.attack1][0])
-                draw_text(text, 20, RED, self.attacks[self.attack1][2].x*TILESIZE, self.attacks[self.attack1][2].y*TILESIZE - 50)
-                text = str(10)
-                draw_text(text, 20, BLUE, self.attacks[self.attack2][2].x*TILESIZE, self.attacks[self.attack2][2].y*TILESIZE - 50)
-                text = str(10)
-                draw_text(text, 20, GREEN, self.attacks[self.attack3][2].x*TILESIZE, self.attacks[self.attack3][2].y*TILESIZE - 50)
-
+                if S in M.allies: 
+                    icon = self.attacks[x][1]
+                    pos = self.attacks[x][2]
+                    rect = pg.Rect(int(pos.x*TILESIZE-49), int(pos.y*TILESIZE-50), 128, 150)
+                    pg.draw.rect(screen,BLACK,rect)
+                    goal_center = (int(pos.x * TILESIZE + TILESIZE / 2), int(pos.y * TILESIZE + TILESIZE / 2))
+                    screen.blit(icon, icon.get_rect(center=goal_center))
+                    text = str(self.attacks[self.attack1][0])
+                    draw_text(text, 20, RED, self.attacks[self.attack1][2].x*TILESIZE, self.attacks[self.attack1][2].y*TILESIZE + 75)
+                    text = str(self.attacks[self.attack2][0])
+                    draw_text(text, 20, BLUE, self.attacks[self.attack2][2].x*TILESIZE, self.attacks[self.attack2][2].y*TILESIZE + 75)
+                    text = str(self.attacks[self.attack3][0])
+                    draw_text(text, 20, GREEN, self.attacks[self.attack3][2].x*TILESIZE, self.attacks[self.attack3][2].y*TILESIZE + 75)
+        def draw_attack(self):
+            pass
 iconaura = [(2, 2), (2, 1), (2, 0), (2, -1), (2, -2), (1, -2), (1, -1), (1, 0), (1, 1), (1, 2), (0, 2), (0, 1), (0, 0), (0, -1), (0, -2), (-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2), (-2, 2), (-2, 1), (-2, 0), (-2, -1), (-2, -2)]            
 H = ally.heplane()
 heplane_combat_img = pg.image.load(os.path.join(filename,'Layer 1_heplane_combat1.png')).convert_alpha()
@@ -221,7 +227,7 @@ H.vec = vec(20,15)
 H.health = 50
 H.shield = 0
 H.combat_animation = {1:heplane_combat_img,2:heplane_combat2_img,3:heplane_combat3_img}
-H.attacks = {H.attack1:[[10,5],heplane_ability1_img,vec(18, 31),[vec(18,31) + a for a in iconaura],False],H.attack2:[[5,0],heplane_ability2_img,vec(23,31),[vec(23,31) + a for a in iconaura],False],H.attack3:[[0,0],heplane_ability3_img,vec(28,31),[vec(28,31)+ a for a in iconaura],True]}
+H.attacks = {H.attack1:[[10,15],heplane_ability1_img,vec(18, 31),[vec(18,31) + a for a in iconaura],False],H.attack2:[[5,0],heplane_ability2_img,vec(23,31),[vec(23,31) + a for a in iconaura],False],H.attack3:[[0,0],heplane_ability3_img,vec(28,31),[vec(28,31)+ a for a in iconaura],True]}
 H.healdam = []
 aura = [(1, 3), (0, 3), (-1, 3), (-1, 2), (0, 2), (1, 2), (1, 1), (0, 1), (-1, 1), (-1, 0), (0, 0), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, -2), (0, -2), (1, -2), (1, -3), (0, -3), (-1, -3)]
 H.clickaura = [H.vec + a for a in aura]
@@ -233,12 +239,12 @@ sri_combat2_img = pg.image.load(os.path.join(filename,'Layer 1_sri_combat2.png')
 sri_combat2_img = pg.transform.scale(sri_combat2_img, (256, 256))
 sri_combat3_img = pg.image.load(os.path.join(filename,'Layer 1_sri_combat3.png')).convert_alpha()
 sri_combat3_img = pg.transform.scale(sri_combat3_img, (256, 256))
-sri_ability1_img = pg.image.load(os.path.join(filename,'cross-1.png.png'))
+sri_ability1_img = pg.image.load(os.path.join(filename,'crystal_icons-2.png.png'))
 sri_ability1_img = pg.transform.scale(sri_ability1_img, (128, 128))
-sri_ability2_img = pg.image.load(os.path.join(filename,'cross-1.png.png'))
+sri_ability2_img = pg.image.load(os.path.join(filename,'crystal_icons-1.png.png'))
 sri_ability2_img = pg.transform.scale(sri_ability2_img, (128, 128))
-sri_ability3_img = pg.image.load(os.path.join(filename,'cross-1.png.png'))
-sri_ability3_img = pg.transform.scale(sri_ability2_img, (128, 128))
+sri_ability3_img = pg.image.load(os.path.join(filename,'crystal_icons-3.png.png'))
+sri_ability3_img = pg.transform.scale(sri_ability3_img, (128, 128))
 S.attack1 = 'flash and crash'
 S.attack2 = 'crystal glass'
 S.attack3 = 'karen and her healing balony'
@@ -246,7 +252,7 @@ S.vec = vec(20,25)
 S.health = 25
 S.shield = 10
 S.combat_animation = {1:sri_combat_img,2:sri_combat2_img,3:sri_combat3_img}
-S.attacks = {S.attack1:[2,sri_ability1_img,vec(18,31),[vec(18,31) + a for a in iconaura],False],S.attack2:[0,sri_ability2_img,vec(23,31),[vec(23,31) + a for a in iconaura],True],S.attack3:[0,sri_ability3_img,vec(28,31),[vec(28,31)+a for a in iconaura],True]}
+S.attacks = {S.attack1:[5,sri_ability1_img,vec(18,31),[vec(18,31) + a for a in iconaura],False],S.attack2:[2,sri_ability2_img,vec(23,31),[vec(23,31) + a for a in iconaura],True],S.attack3:[10,sri_ability3_img,vec(28,31),[vec(28,31)+a for a in iconaura],True]}
 S.clickaura = [S.vec + a for a in aura]
 
 
@@ -312,15 +318,19 @@ class main():
     def draw_damage(self):
         if self.display == True:
             for x in self.damage:
-                if len(self.damage[x]) == 2:
-                    draw_text(str(self.damage[x][0]),30,RED,self.allies[x][0].x*TILESIZE-15, self.allies[x][0].y*TILESIZE-150,align="bottomright")
-                    draw_text(str(self.damage[x][1]),30,RED,self.allies[x][0].x*TILESIZE+15, self.allies[x][0].y*TILESIZE-150,align="bottomright")
-                else:
-                    draw_text(str(self.damage[x][0]),30,RED,self.allies[x][0].x*TILESIZE, self.allies[x][0].y*TILESIZE-150,align="bottomright")    
+                if x in self.allies:
+                    if len(self.damage[x]) == 2:
+                        print(self.allies[x][0])
+                        draw_text(str(self.damage[x][0]),30,RED,self.allies[x][0].x*TILESIZE-15, self.allies[x][0].y*TILESIZE-150,align="bottomright")
+                        draw_text(str(self.damage[x][1]),30,RED,self.allies[x][0].x*TILESIZE+15, self.allies[x][0].y*TILESIZE-150,align="bottomright")
+                    else:
+                        draw_text(str(self.damage[x][0]),30,RED,self.allies[x][0].x*TILESIZE, self.allies[x][0].y*TILESIZE-150,align="bottomright")    
     def draw_background(self):
         goal_center = int(WIDTH / 2), int(HEIGHT/ 2)
         screen.blit(background_fall, background_fall.get_rect(center=goal_center))
-        
+    def draw_attack(self):
+        self.selectedchar.draw_attack()
+        pass
     def checkifdead(self):
         test = dict(self.enemy)
         for x in test:
@@ -330,7 +340,49 @@ class main():
         for x in test:
             if test[x][1] <= 0:
                 del self.allies[x]
-        if len(self.allies) <= 0 or len(self.enemy) <= 0:
+        if len(self.enemy) <= 0:
+            #ally1 = H
+            #ally2 = S
+
+            self.selectedattack = 0
+            self.selectedchar = 0
+            self.current_animation = 1
+            #self.allies = {}
+            self.enemy = {}
+            self.l = []
+            self.actions = []
+
+            #self.ally1 = ally1
+            #pos = self.ally1.vec
+            #eat = self.ally1.health
+            #lean = self.ally1.shield
+            #self.allies.update({self.ally1:[pos,eat,self.ally1.clickaura,lean]})
+
+            #self.ally2 = ally2
+            #pos = self.ally2.vec
+            #eat = self.ally2.health
+            #lean = self.ally2.shield
+            #self.allies.update({self.ally2:[pos,eat,self.ally2.clickaura,lean]})
+            for x in range(1,3):#range(1,random.randint(2,3))
+                if x == 1:
+                    self.enemy1 = random.choice([C,mage])
+                    tout = self.enemy1.vec
+                    eat = self.enemy1.health
+                    attack = self.enemy1.attacks
+                    M.enemy.update({self.enemy1:[tout,eat,[tout + x for x in self.enemy1.clickaura],attack]})
+                if x == 2:
+                    self.enemy2 = random.choice([C,mage])
+                    if self.enemy2 == self.enemy1:
+                        if self.enemy2 == mage:
+                            self.enemy2 = mage2
+                        if self.enemy2 == C:
+                            self.enemy2 = C2
+                    tout = self.enemy2.vec
+                    eat = self.enemy2.health
+                    attack = self.enemy2.attacks
+                    M.enemy.update({self.enemy2:[tout,eat,[tout + x for x in self.enemy2.clickaura],attack]})
+            self.numberofenemy()
+        if len(self.allies) <= 0:
             ally1 = H
             ally2 = S
 
@@ -456,6 +508,7 @@ M.allies = {}
 M.enemy = {}
 M.l = []
 M.actions = []
+M.targetedenemy = 0
 background_fall = pg.image.load(os.path.join(filename,'backgorunds-2.png.png'))
 background_fall = pg.transform.scale(background_fall, (1920, 1080))
 
@@ -552,13 +605,14 @@ while running:
                             
                     else:
                         pass 
+                elif mpos not in M.getaura() and mpos not in M.selectingattack():
+                    M.selectedchar = 0
+                    M.attackselect = False
                 if mpos in M.selectingattack():
                     M.selectattack()
                 if mpos in M.selectingchar() and M.attackselect == False:
                     M.selectchar()
-                if mpos not in M.getaura() and mpos not in M.selectingattack():
-                    M.selectedchar = 0
-                    M.attackselect = False
+                
                 create.append(mpos)
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_r:
@@ -588,7 +642,7 @@ while running:
         M.enemycanattack = False
         M.attackselect = False
         M.checkifdead()
-    if len(M.actions) == len(M.allies) and M.enemycanattack == False:
+    if len(M.actions) >= len(M.allies) and M.enemycanattack == False:
         attck_timer = pg.time.get_ticks()
         M.enemycanattack = True
         M.actions = []
