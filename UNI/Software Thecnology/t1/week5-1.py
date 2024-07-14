c733842729084de96de8002c08abffd8591456bd
tomwaganagado <burton@vanderstok.net>
2024-07-14 16:03:04 +1000

    14/07/2024

--- UNI/Software Thecnology/t1/week5-1.py
+++ UNI/Software Thecnology/t1/week5-1.py
@@ -16,20 +16,32 @@ class MyGUI:
         f = Frame(self.main_window)
         f.pack(padx=10)
         tkinter.Button(f,text="Show Laps",command=self.showlaps).pack(side="left")
+        tkinter.Button(f,text="delete laps",command=self.deletelaps).pack(side="left")
         tkinter.mainloop()
+    def deletelaps(self):
+        for widget in self.f.winfo_children():
+            widget.destroy()
+        self.f.pack_forget()
+        for widget in self.f1.winfo_children():
+            widget.destroy()
+        self.f1.pack_forget()
     def showlaps(self):
+        self.f = Frame(self.main_window)
+        self.f.pack(padx=10)
         for x in range(self.second.get()):
-            f = Frame(self.main_window)
-            f.pack(padx=10)
+            f = Frame(self.f,bg="red")
+            f.pack(padx=20)
             self.first.append(tkinter.IntVar(self.main_window))
             b1 = tkinter.Label(f,text=f"Lap {x+1} time:")
             b2 = tkinter.Entry(f,textvariable= self.first[x])
             b1.pack(side="left")
             b2.pack(side="left")
-        f = Frame(self.main_window)
+        f = Frame(self.f)
         f.pack(padx=10)
         tkinter.Button(f,text="Calculate Times",command=self.showinformation).pack(side="left")
     def showinformation(self):
+        self.f1 = Frame(self.main_window)
+        self.f1.pack(padx=10)
         a = 0
         s = float("inf")
         l = 0
@@ -43,9 +55,9 @@ class MyGUI:
             if t > l:
                 l = t
         a /= len(self.first)
-        tkinter.Label(self.main_window,text=f"Average lap time: {a}").pack()
-        tkinter.Label(self.main_window,text=f"Shortest lap time: {s}").pack()
-        tkinter.Label(self.main_window,text=f"Longest lap time: {l}").pack()
+        tkinter.Label(self.f1,text=f"Average lap time: {a}").pack()
+        tkinter.Label(self.f1,text=f"Shortest lap time: {s}").pack()
+        tkinter.Label(self.f1,text=f"Longest lap time: {l}").pack()
         
         
 MyGUI()
